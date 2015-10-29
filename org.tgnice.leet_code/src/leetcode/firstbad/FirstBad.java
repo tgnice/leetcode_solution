c15dca2fa7acebaf1ff66c3dc4bf9e6dd17a7900
tgnice <tgnice@gmail.com>
2015-10-30 04:00:13 +0900

    added First Bad Solution

---
+++ org.tgnice.leet_code/src/leetcode/firstbad/FirstBad.java
@@ -0,0 +1,42 @@
+package leetcode.firstbad;
+/* The isBadVersion API is defined in the parent class VersionControl.
+boolean isBadVersion(int version); */
+
+public class FirstBad {
+    public int firstBadVersion(int n) {
+    	return checkBad(n/2, n);	
+    }
+    
+    private int checkBad(int current , int max){
+    	System.out.println("current = " + current + ", max = "+ max);
+    	if(max <2){
+    		return 1;
+    	}else {
+	    	if(isBadVersion(current) == false && isBadVersion(current -1) == false){
+	    		if(max-current == 1){
+	    			current = current +1;
+	    		}
+	    		return checkBad(current + (max-current)/2, max);
+	    	} else if(isBadVersion(current) == true && isBadVersion(current -1) == false){
+	    		return current;
+	    	} else {
+	    		return checkBad(max - current/2, current);
+	    	}
+    	}
+    }
+  /* below is not a part of solution
+   * 
+   * 
+   * */  
+    private boolean isBadVersion(int version){
+    	if(version >=1702766719){
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public static void main(String[] args){
+    	FirstBad fb = new FirstBad();
+    	System.out.println(fb.firstBadVersion(2126753390));
+    }
+}
